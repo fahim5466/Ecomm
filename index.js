062ef355d048dc3df10308a6a8e45be9b655e2df
Raihan Kabir Fahim <fahimraihan6@gmail.com>
2024-05-13 19:39:15 +0600

    Products can now be added to cart. Added products can be viewed in cart page.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
 const adminProductsRouter = require('./routes/admin/products');
 const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(cookieSession({
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
     console.log("Server is listening...");
